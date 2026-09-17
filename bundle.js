f341790bfd599d2c0a8ddb440f6fc06e7bd6efa5
ps4 <elhainaamine2@gmail.com>
2026-09-17 13:41:53 +0100

    Delete bundle.js

--- bundle.js
+++
@@ -1,9 +0,0 @@
-
-// Used by the second rollup pass to reassemble the code-split chunks into a single bundle.
-import './out/chunk1.js';
-import './out/worker.js';
-import './out/main.js';
-
-export default lib;
-
-
